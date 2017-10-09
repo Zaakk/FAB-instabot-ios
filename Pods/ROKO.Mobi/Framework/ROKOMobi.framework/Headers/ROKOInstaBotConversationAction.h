b57926b3f1105c37614cfa16ece30614f8bbf916
Alexander Zakatnov <alexander.zakatnov@rokolabs.com>
2017-10-09 17:04:50 +0400

    init commit

---
+++ Pods/ROKO.Mobi/Framework/ROKOMobi.framework/Headers/ROKOInstaBotConversationAction.h
@@ -0,0 +1,36 @@
+//
+//  ROKOInstaBotConversationAction.h
+//  ROKOMobi
+//
+//  Created by Alexey Golovenkov on 14.09.16.
+//  Copyright © 2016 ROKO Labs. All rights reserved.
+//
+
+#import "ROKODataObject.h"
+
+typedef NS_ENUM (NSInteger, ROKOInstaBotConversationActionType) {
+	ROKOInstaBotConversationActionTypeTextPrompt,
+	ROKOInstaBotConversationActionTypeLink,
+	ROKOInstaBotConversationActionTypeAppPage,
+	ROKOInstaBotConversationActionTypeShare,
+	ROKOInstaBotConversationActionTypeClose,
+    ROKOInstaBotConversationActionTypeReference
+};
+
+@class ROKOInstaBotConversationTreeNode;
+@class ROKOImageDataObject;
+
+@interface ROKOInstaBotConversationAction : ROKODataObject
+
+@property (nonatomic, assign) ROKOInstaBotConversationActionType type;
+@property (nonatomic, assign) ROKOInstaBotConversationActionType postActionType;
+@property (nonatomic, copy) NSString *buttonText;
+@property (nonatomic, strong) ROKOInstaBotConversationTreeNode *childNode;
+@property (nonatomic, strong) NSDictionary *pageSettings;
+@property (nonatomic, copy) NSString *url;
+@property (nonatomic, strong) NSString *cardImageURL;
+@property (nonatomic, copy) NSString *cardDescription;
+@property (nonatomic, copy) NSArray *multiSelectOptions;
+
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary saveNodesToMap:(NSMutableDictionary<NSNumber *, ROKOInstaBotConversationTreeNode *> *)map;
+@end
